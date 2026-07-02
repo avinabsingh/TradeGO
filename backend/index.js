0b9b17c11af7cc007b9657411cc2015f2ae3d718
avinabsingh <avinabsingh2403@gmail.com>
2026-07-03 00:37:22 +0530

    additional changes

--- backend/index.js
+++ backend/index.js
@@ -22,7 +22,8 @@ app.use(cookieParser());
 app.use(
   cors({
     origin: ["http://localhost:3000",
-      "http://localhost:3001",],
+      "http://localhost:3001","https://trade-go-one.vercel.app",
+      "https://trade-go-fuma-nine.vercel.app"],
     credentials: true,
   })
 );
